b720b89d3ef59f48a26d8e3d7a1cd6cb09c047af
Will <w255li@edu.uwaterloo.ca>
2019-07-26 17:42:01 -0400

    made posteriors but does not work

--- tree.py
+++ tree.py
@@ -1,3 +1,7 @@
+import numpy as np
+
+LIKELIHOODS = [[0,0.4096,0.2592,0.0768,0.0064],[0,0.3,0.423,0.04,0.23],[0,123,0.84,0.001,0.3],[0,0.4096,0.2592,0.0768,0.0064],[0,0.4096,0.2592,0.0768,0.0064],[0,0.4096,0.2592,0.0768,0.0064]]
+
 class node:
     def __init__(self,p,c,t):
 
@@ -5,10 +9,10 @@ class node:
         self.children = c
         self.node_type = t
         self.priors = []
-        self.posterors = []
+        self.posteriors = []
 
     def __str__(self):
-    	return 'Parent '+str(self.parent)+' Children '+str(self.children)
+    	return 'Parent '+str(self.parent)+' Children '+str(self.children) + ' posteriors ' + str(self.posteriors) + 'nodeType ' + str(self.node_type)
 
     def add_child(self,node):
     	self.children.append(node)
@@ -21,9 +25,17 @@ class tree:
 		self.list_of_nodes.append(node([],[1,2],'root'))
 		self.list_of_nodes.append(node([0],[],'market'))
 		self.list_of_nodes.append(node([0],[],'survey'))
+		self.list_of_nodes[2].posteriors=[0.2,0.2,0.2,0.2,0.2]
 
 
 	def add_node(self,node):
+		priors = self.list_of_nodes[node.parent].posteriors
+		if (node.node_type in [0,1,2,3,4,5]):
+			joint_probablity = np.multiply(priors,LIKELIHOODS[node.node_type])
+			sum_joint = sum(joint_probablity)
+			node.posteriors = [joint_probablity/sum_joint]
+		else:
+			node.posteriors = priors
 		self.list_of_nodes.append(node)
 		self.list_of_nodes[node.parent].children.append(len(self.list_of_nodes)-1)
 
@@ -55,7 +67,10 @@ def add_layer(tree,layer_type):
 				for k in range(6):
 					tree.add_node(node(i,[],k))
 
-				
+n= node([1],[1],3)
+
+if( n.node_type in [1,2,3,4,5] ):
+	print('d')
 
 add_layer(t,'outcome')
 add_layer(t,'decision')
