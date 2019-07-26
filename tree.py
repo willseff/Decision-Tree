6fbab46f7ecc1d4dfb6cc9a39ca052b13b4a6f6b
Will <w255li@edu.uwaterloo.ca>
2019-07-26 16:48:09 -0400

    working tree

--- tree.py
+++ tree.py
@@ -4,6 +4,8 @@ class node:
         self.parent = p
         self.children = c
         self.node_type = t
+        self.priors = []
+        self.posterors = []
 
     def __str__(self):
     	return 'Parent '+str(self.parent)+' Children '+str(self.children)
@@ -20,9 +22,10 @@ class tree:
 		self.list_of_nodes.append(node([0],[],'market'))
 		self.list_of_nodes.append(node([0],[],'survey'))
 
+
 	def add_node(self,node):
 		self.list_of_nodes.append(node)
-		self.list_of_nodes[node.parent].children.append(len(self.list_of_nodes))
+		self.list_of_nodes[node.parent].children.append(len(self.list_of_nodes)-1)
 
 	def __str__(self):
 		string = ''
@@ -30,7 +33,7 @@ class tree:
 			string = string + ' node ' +str(i) + ' ' + (str(self.list_of_nodes[i]) + '\n')
 		return string
 
-	def get_node(self,number):
+	def node(self,number):
 		return self.list_of_nodes[number]
 
 	def len(self):
@@ -42,15 +45,13 @@ t = tree()
 def add_layer(tree,layer_type):
 	if (layer_type == 'decision'):
 		for i in range(tree.len()):
-			if (not tree.get_node(i).children and tree.get_node(i).node_type != 'market'):
-
+			if (not tree.node(i).children and tree.node(i).node_type != 'market'):
 				tree.add_node(node(i,[],'survey'))
 				tree.add_node(node(i,[],'market'))
 
-		print('k')
 	if (layer_type == 'outcome'):
 		for i in range(tree.len()):
-			if (not tree.get_node(i).children and tree.get_node(i).node_type == 'survey'):
+			if (not tree.node(i).children and tree.node(i).node_type == 'survey'):
 				for k in range(6):
 					tree.add_node(node(i,[],k))
 
@@ -58,4 +59,10 @@ def add_layer(tree,layer_type):
 
 add_layer(t,'outcome')
 add_layer(t,'decision')
+add_layer(t,'outcome')
+add_layer(t, 'decision')
+add_layer(t,'outcome')
+add_layer(t, 'decision')
+add_layer(t,'outcome')
+add_layer(t, 'decision')
 print(t)
