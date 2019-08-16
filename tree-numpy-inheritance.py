4e5f1cc2e8f66765a08132c707da6b70ee408d1e
Will <w255li@edu.uwaterloo.ca>
2019-08-15 23:01:35 -0400

    changed lists to numpy arrays

--- tree-numpy-inheritance.py
+++ tree-numpy-inheritance.py
@@ -3,10 +3,10 @@ import scipy.stats as ss
 import csv
 
 MARKETVALUE = 10000000
-PRIORS = [0.2,0.2,0.2,0.2,0.2]
-MARKETSHARES = [0.3,0.4,0.5,0.6,0.7]
+PRIORS = np.array([0.2,0.2,0.2,0.2,0.2])
+MARKETSHARES = np.array([0.3,0.4,0.5,0.6,0.7])
 SURVEYCOST = 10250
-LIKELIHOODS = [[0.1176,0.0466,0.01563,0.0041,0.00073],[0.3,0.18,0.093,0.036,0.01],[0.32,0.311,0.234,0.14,0.059],[0.185,0.276,0.312,0.276,0.18],[0.059,0.138,0.234,0.311,0.324],[0.01,0.0368,0.093,0.186,0.3]]
+LIKELIHOODS = np.array([[0.1176,0.0466,0.01563,0.0041,0.00073],[0.3,0.18,0.093,0.036,0.01],[0.32,0.311,0.234,0.14,0.059],[0.185,0.276,0.312,0.276,0.18],[0.059,0.138,0.234,0.311,0.324],[0.01,0.0368,0.093,0.186,0.3]])
 
 class Node:
     node_number = 1
@@ -35,32 +35,43 @@ class Node:
         for child in self.children:
             string += str(child) + ' '
         return string
+
+
+
 class DecisionNode(Node):
     def __init__ (self,parent,value =''):
-
         super().__init__(parent=parent)
-
         self.value = value
 
     @property
     def posteriors(self):
-        return self.parent.posteriors
+        return np.copy(self.parent.posteriors)
     
 
+
+
+
 class OutcomeNode(Node):
-    def __init__ (self,value,parent):
-
+    def __init__ (self,parent,value):
         super().__init__(parent=parent)
-
         self.value=value
 
     @property
     def posteriors(self):
+        print(self)
         priors = self.parent.posteriors
+        print(priors)
         joint_probablity = priors*LIKELIHOODS[self.value]
-        sum_joint = sum(joint_probablity)
-        posteriors = joint_probablity/sum_joint
+        print(LIKELIHOODS[self.value])
+        print(joint_probablity)
+        print(sum(joint_probablity))
+        posteriors = joint_probablity/sum(joint_probablity)
+        print(posteriors)
+        print('wtf')
         return posteriors
+
+
+
 
 class EndNode(Node):
     def __init__ (self,parent,prior_surveys=0):
@@ -69,7 +80,16 @@ class EndNode(Node):
 
     @property
     def posteriors(self):
-    	return self.parent.posteriors
+    	return np.copy(self.parent.posteriors)
+
+    # @property
+    # def expected_value(self):
+    # 	expected_value = self.posteriors
+    # 	return 
+    
+
+
+
     
 
 class Tree(list):
@@ -82,7 +102,7 @@ class Tree(list):
         self.add_node(parent = self[0],node_class= DecisionNode, value= 'market')#node 1
         self.add_node(parent = self[0], node_class = DecisionNode, value = 'survey')#node 2
         for i in range(6): #nodes 3 to 7
-        	self.add_node(parent = self[2], node_class = OutcomeNode, value = 1) 
+        	self.add_node(parent = self[2], node_class = OutcomeNode, value = i) 
 
     def add_layer(self):
         for node in self:
@@ -101,11 +121,16 @@ class Tree(list):
         parent.add_child(new_node)
         Node.increment_node()
 
+    def posters(self):
+   	    for node in self:
+   	        node.posteriors
+
     def print_tree(self):
         for node in self:
+        
             print(f'node number {node}  node type {node.value}')
             print (f'chidren {node.see_children()}, parent {str(node.parent)}')
-            print(f'posteriors {node.posteriors}')
+            print(f'posteriors {node.posteriors}, ')
 
 
 
@@ -114,7 +139,7 @@ class Tree(list):
 
 t = Tree()
 t.add_layer()
-t.print_tree()
+t.posters()
 #print(id(t[1].parent), id(t[0]))
 #print(t[5])
 
